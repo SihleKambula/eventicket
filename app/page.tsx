40a96aeca044a7cfd6f5b4751885e87be981345d
Sihle Kambula <kambulasihle@gamil.com>
2023-07-25 22:18:01 +0200

    custom theme and navbar set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { useEffect } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/config";
+import { Container, Box, Card, Typography } from "@mui/material";
 export default function Home() {
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "events"), (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        console.log(doc.data());
+      });
+    });
+    return () => unsub();
+  }, []);
+
   return (
     <main>
-      <h1>Hello Project</h1>
+      <Container>
+        <Box>
+          <Card>
+            <Typography variant='h2'>Hello Sihle</Typography>
+          </Card>
+        </Box>
+      </Container>
     </main>
   );
 }
